dd80fa84bdbe9a05d47c97be955e207ec2245b3f
Frank E Weiss <feweiss@gmail.com>
2022-12-08 13:57:18 -0800

    Implement read and write register

--- components/canbus/inc/canbus/mcp25625.h
+++ components/canbus/inc/canbus/mcp25625.h
@@ -9,4 +9,15 @@ public:
 
     void init();
     void registerTest(spi_device_handle_t spi);
+    void readRegister(uint8_t const address, uint8_t & value);
+    void writeRegister(uint8_t const address, uint8_t const value);
+private:
+    // this is the device object
+    spi_device_handle_t spi;
+
+    // the spi interface requires int16_t, but mcp uses only uint8_t
+    enum cmd {
+        READ = 0x03,
+        WRITE = 0x02,
+    };
 };
--- components/canbus/mcp25625.cpp
+++ components/canbus/mcp25625.cpp
@@ -35,17 +35,26 @@ void MCP25625::init() {
     };
 
     esp_err_t err;
-    const spi_host_device_t canHost = VSPI_HOST;
+    const spi_host_device_t canHost = HSPI_HOST;
     err = spi_bus_initialize(canHost, &buscfg, SPI_DMA_CH_AUTO);
     ESP_ERROR_CHECK(err);
 
-    spi_device_handle_t spi;
     err = spi_bus_add_device(canHost, &devcfg, &spi);
     ESP_ERROR_CHECK(err);
 
     ESP_LOGI(TAG, "spi device configured");
 
-    registerTest(spi);
+    // registerTest(spi);
+    const uint8_t CANCTRL = 0x0e;
+    uint8_t value;
+    readRegister(CANCTRL, value);
+    ESP_LOGI(TAG, "register %x is %x", CANCTRL, value);
+
+    const uint8_t CNF1 = 0x2a;
+    writeRegister(CNF1, 0x11);
+    uint8_t v2;
+    readRegister(CNF1, v2);
+    ESP_LOGI(TAG, "read back %x", v2);
 
     err = spi_bus_remove_device(spi);
     ESP_ERROR_CHECK(err);
@@ -53,13 +62,31 @@ void MCP25625::init() {
     err = spi_bus_free(canHost);
     ESP_ERROR_CHECK(err);
 }
-void MCP25625::registerTest(spi_device_handle_t spi) {
+// void MCP25625::registerTest(spi_device_handle_t spi) {
+//     esp_err_t err;
+
+//     const uint8_t MCP_READ = 0x03;  
+//     spi_transaction_t readmcp {};
+//     readmcp.cmd = MCP_READ;
+//     readmcp.addr = 0x0f;
+//     readmcp.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA; // | SPI_TRANS_MODE_OCT;
+//     readmcp.length = 8;
+//     readmcp.rxlength = 8;
+//     err = spi_device_queue_trans(spi, &readmcp, portMAX_DELAY);
+//     ESP_ERROR_CHECK(err);
+
+//     spi_transaction_t  * readmcpresult {};
+//     err = spi_device_get_trans_result(spi, &readmcpresult, portMAX_DELAY);
+//     ESP_ERROR_CHECK(err);
+//     ESP_LOGI(TAG, "read: %d %x %x %x %x", readmcpresult->rxlength, readmcpresult->rx_data[0],  readmcpresult->rx_data[1],  readmcpresult->rx_data[2],  readmcpresult->rx_data[3]);
+// }
+
+void MCP25625::readRegister(uint8_t const address, uint8_t & value) {
     esp_err_t err;
 
-    const uint8_t MCP_READ = 0x03;  
     spi_transaction_t readmcp {};
-    readmcp.cmd = MCP_READ;
-    readmcp.addr = 0x0f;
+    readmcp.cmd = cmd::READ;
+    readmcp.addr = address;
     readmcp.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA; // | SPI_TRANS_MODE_OCT;
     readmcp.length = 8;
     readmcp.rxlength = 8;
@@ -69,5 +96,26 @@ void MCP25625::registerTest(spi_device_handle_t spi) {
     spi_transaction_t  * readmcpresult {};
     err = spi_device_get_trans_result(spi, &readmcpresult, portMAX_DELAY);
     ESP_ERROR_CHECK(err);
-    ESP_LOGI(TAG, "read: %d %x %x %x %x", readmcpresult->rxlength, readmcpresult->rx_data[0],  readmcpresult->rx_data[1],  readmcpresult->rx_data[2],  readmcpresult->rx_data[3]);
+    // ESP_LOGI(TAG, "read: %d %x %x %x %x", readmcpresult->rxlength, readmcpresult->rx_data[0],  readmcpresult->rx_data[1],  readmcpresult->rx_data[2],  readmcpresult->rx_data[3]);
+    value = readmcpresult->rx_data[0];
 }
+void MCP25625::writeRegister(uint8_t const address, uint8_t const value) {
+    esp_err_t err;
+
+    spi_transaction_t readmcp {};
+    readmcp.cmd = cmd::WRITE;
+    readmcp.addr = address;
+    readmcp.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA; // | SPI_TRANS_MODE_OCT;
+    readmcp.length = 8;
+    readmcp.rxlength = 0;
+    readmcp.tx_data[0] = value;
+    err = spi_device_queue_trans(spi, &readmcp, portMAX_DELAY);
+    ESP_ERROR_CHECK(err);
+
+    // TODO try spi_device_transmit
+    spi_transaction_t  * readmcpresult {};
+    err = spi_device_get_trans_result(spi, &readmcpresult, portMAX_DELAY);
+    ESP_ERROR_CHECK(err);
+    // ESP_LOGI(TAG, "read: %d %x %x %x %x", readmcpresult->rxlength, readmcpresult->rx_data[0],  readmcpresult->rx_data[1],  readmcpresult->rx_data[2],  readmcpresult->rx_data[3]);
+}
+
